508519379829e5824b209d05ac925ca6ad0f49ac
Andrew Williams <andrew@manor.org>
2012-04-21 08:47:35 -0400

    Cleanup.
    Comments.
    
    Main readloop done.  Hook for storage done.  No actual storage.
    
    Need webserver etc.

--- FlowTrack.pl
+++ FlowTrack.pl
@@ -4,25 +4,35 @@ use warnings;
 use IO::Socket::INET;
 use Net::Flow qw(decode) ;
 use Data::Dumper;
+use Net::IP;
 use POE;
 use autodie;
 
 # Some configuration
 my $PORT = 2055;
 my $DATAGRAM_LEN = 1548;
+my $PURGE_INTERVAL = 15;
 
 
-POE::Session->create 
-(
- inline_states => {
-     _start => \&server_start,
-     get_datagram => \&server_read,
- }
-);
-POE::Kernel->run();
-exit 0;
+main();
+sub main
+{
+    POE::Session->create (
+	inline_states => {
+	    _start => \&server_start,
+	    get_datagram => \&server_read,
+	    store_data => \&store_data,
+	 }
+    );
+    POE::Kernel->run();
+    exit 0;
+}
 
 
+#
+# Main init code.  Start the listen loop, setup the data store job
+# Evcentually will fork off the webserver
+#
 sub server_start
 {
     my $kernel = $_[KERNEL];
@@ -32,13 +42,39 @@ sub server_start
     );
 
 
-    print "Starting up bizatch....\n";
+    # Send a delayed message to store data
+    $kernel->delay(store_data => $PURGE_INTERVAL);
+
+    # Start off the select read.  use the get_datagram message
     $kernel->select_read($socket, "get_datagram");
-    
+}
+
+
+#
+# Do something with the data we've collected.  Reads the cached data out of heap and stores it.  
+# Called periodically (PURGE_INTERVAL) with a delayed message setup in server_start.  MUST RE-INJECT 
+# It's own delay.  (much like alarm)
+#
+sub store_data
+{
+    my $kernel = $_[KERNEL];
+    my $flow_data = $_[HEAP]->{'flows'};
+
+    print Dumper($flow_data);
+
+    # We've processed the flows, clear the heap for the next batch
+    delete $_[HEAP]->{'flows'};
+
+    # Restart the timer
+    $kernel->delay(store_data => $PURGE_INTERVAL);
 }
 
 
 
+#
+# This is where we read and process the netflow packet
+# get_datagram event handler
+#
 sub server_read
 {
     my ($kernel, $socket) = @_[KERNEL, ARG0];
@@ -48,23 +84,71 @@ sub server_read
 
     my $remote_address = $socket->recv($packet, $DATAGRAM_LEN);
 
-    my($peer_port, $peer_addr) = unpack_sockaddr_in($remote_address);
-    my $human_remote = inet_ntoa($peer_addr);
-    
-    print "msg from: $human_remote ($peer_port)\n";
-    
-     ( $HeaderHashRef,
-       $TemplateArrayRef,
-       $FlowArrayRef,
-       $ErrorsArrayRef)
-	 = Net::Flow::decode(
-	 \$packet,
-	 $TemplateArrayRef
-	 ) ;
 
-    print Dumper($FlowArrayRef);
+    # Get the decoded flow (is a list)
+    my $decoded_packet = decode_packet($packet);
+
+    # Put the decoded flow onto the HEAP for later storage (via store_data)
+    push( @{$_[HEAP]->{'flows'}}, @$decoded_packet);
+
+}
+
+#
+# Actually do the packet decode
+#
+sub decode_packet
+{
+    my ($packet) = @_;
+    my $TemplateArrayRef = undef;
+    my ($HeaderHashRef, $FlowArrayRef, $ErrorsArrayRef) = ();
+
+     ( $HeaderHashRef, $TemplateArrayRef, $FlowArrayRef, $ErrorsArrayRef)
+	 = Net::Flow::decode(\$packet, $TemplateArrayRef ) ;
+
+    return decode_netflow($FlowArrayRef);
 }
 
 
+#
+# Make a usable datastructure out of the data from decode_packet
+# Since we can get multiple records we're returning a list here
+#
+sub decode_netflow
+{
+    my ($flow_struct) = @_;
+
+    my $ret_list = [];
+
+    foreach my $flow (@{$flow_struct})
+    {
+	my $tmp_struct = {};
+
+	# The indicies of the data in $flow is documented in the netflow library
+	# kind of a dumb way to do this, but it's not my module
+	$tmp_struct->{src_ip} = inet_ntoa($flow->{'8'});
+	$tmp_struct->{dst_ip} = inet_ntoa($flow->{'12'});
+	$tmp_struct->{src_prt} = hex(unpack("H*", $flow->{'7'}));
+	$tmp_struct->{dst_prt} = hex(unpack("H*", $flow->{'11'}));
+	$tmp_struct->{bytes} = hex(unpack("H*", $flow->{'1'}));
+	$tmp_struct->{packets} = hex(unpack("H*", $flow->{'2'}));
+
+	push(@{$ret_list}, $tmp_struct);
+
+    }
+
+    return $ret_list
+}
 
 
+#
+# Make a Net::IP object
+#
+sub getIP
+{
+    my $raw_ip = shift();
+    my $ip;
+
+    $ip = new Net::IP(inet_ntoa($raw_ip)) || die "Couldn't create net object";
+
+    return $ip;
+}
