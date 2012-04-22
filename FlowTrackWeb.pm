54c36a52a033b19400dcaa8100dc24412ff4f71c
Andrew Williams <andrew@manor.org>
2012-04-22 08:50:24 -0400

    Basic webservice appears to be working

---
+++ FlowTrackWeb.pm
@@ -0,0 +1,34 @@
+package FlowTrackWeb;
+
+use POE;
+use POE::Component::Server::HTTP;
+use HTTP::Status qw(:constants);
+
+
+# This starts the server
+# Should be called via WheelRun from FlowTrack.pm
+sub ServerStart
+{
+    POE::Kernel->stop();
+    
+    
+    warn "Starting server";
+    
+    $httpd = POE::Component::Server::HTTP->new 	(
+	Port => 8000,
+	ContentHandler => { '/' => \&homepage },
+	Headers => { Server => 'My Server' },
+	);
+
+    POE::Kernel->run();
+
+}
+
+sub homepage
+{
+    my ($request, $response) = @_;
+    $response->code(RC_OK);
+    $response->content("Hi, you fetched ". $request->uri);
+    return RC_OK;
+}
+1;
