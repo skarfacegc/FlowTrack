1c3f958ce0664dc6a30bf716ce5e76b1155acb22
Andrew Williams <andrew@manor.org>
2012-09-02 11:00:40 -0400

    Added a static html directory

--- FT/FlowTrackWeb.pm
+++ FT/FlowTrackWeb.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Carp;
 use Mojo::Base 'Mojolicious';
+use Mojolicious::Static;
 use Data::Dumper;
 use vars '$AUTOLOAD';
 
@@ -11,12 +12,17 @@ sub startup
 {
     my $self = shift();
 
+    # Serve up the static pages
+    $self->static( Mojolicious::Static->new() );
+    push( @{ $self->static->paths }, './html' );
+
     my $r = $self->routes;
 
-    $r->route('/')->name('index')->to(controller=>'main', action=>'index');
+    $r->route('/')->name('index')->to( controller => 'main', action => 'index' );
 
-    $r->route('/FlowsForLast/:timerange')->to(controller=>'main', action=>'simpleFlows');
-    $r->route('/json/FlowsForLast/:timerange')->to(controller=>'main',action=>'simpleFlowsJSON');
+    $r->route('/FlowsForLast/:timerange')->to( controller => 'main', action => 'simpleFlows' );
+    $r->route('/json/FlowsForLast/:timerange')->to( controller => 'main', action => 'simpleFlowsJSON' );
+
 }
 
 1;
