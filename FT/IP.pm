db9b53514696ba0457c6216258b39cfe98c059a0
Andrew Williams <andrew@manor.org>
2013-01-03 23:56:55 -0500

    Made getIPObj a bit smarter.  Can now take either an integer or a dotted quad

--- FT/IP.pm
+++ FT/IP.pm
@@ -1,3 +1,7 @@
+# This contains some basic IP utils
+# Mainly here so we can memoize some of the IP operations
+#
+
 package FT::IP;
 use warnings;
 use strict;
@@ -5,10 +9,25 @@ use Net::IP;
 use Memoize;
 
 memoize('getIPObj');
+
+# Returns an IP object for the provided IP
+# tries to determine if it's a intip or a dotted quad
+#  yes the check is actually crappy  (just checks for an integer or non integer)
+#  but it should be ok in this context
 sub getIPObj
 {
     my ($ip) = @_;
-    return Net::IP->new( join( '.', unpack( 'C4', pack( "N", $ip ) ) ) );
+
+    # If we get a raw integer
+    if ( $ip =~ /^\d+$/ )
+    {
+        return Net::IP->new( join( '.', unpack( 'C4', pack( "N", $ip ) ) ) );
+    }
+    else
+    {
+        return Net::IP->new($ip);
+    }
+}
 }
 
 1;
